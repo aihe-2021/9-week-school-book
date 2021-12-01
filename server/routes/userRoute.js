5ccd2f1c4c72f2e52fbea556685cbaa9e6477ada
iamjojo <jojolsexton@gmail.com>
2021-12-02 11:56:50 +1300

    db function and api on server side

---
+++ server/routes/userRoute.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const db = require('../db/users')
+const router = express.Router()
+const { upload, uploadImage } = require('../controllers/userpiccontroller')
+
+// GET ALL USERS
+router.get('/', (req, res) => {
+  db.getUsers()
+    .then(users => {
+      res.json(users)
+      return null
+    })
+    .catch(e => {
+      console.log(e.message)
+      res.status(500).send('database error')
+    })
+})
+
+// GET ONE USER:  http://localhost:3000/api/v1/users/1
+router.get('/:id', (req, res) => {
+  db.getOne(req.params.id)
+    .then(user => res.json(user))
+    .catch(e => {
+      console.log(e.message)
+    })
+})
+
+// UPLOAD PHOTO:
+router.post('/upload', uploadImage, upload)
+
+module.exports = router
