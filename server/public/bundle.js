6e5d926d4aa506441841b5bb7a5bb56aa4b21d0d
Danny <Danny@Apples-iMac.local>
2021-12-02 15:24:28 +1300

    natalia css merged

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -2371,7 +2371,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 
 
-
 function Intro() {
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
     className: "intro__container"
