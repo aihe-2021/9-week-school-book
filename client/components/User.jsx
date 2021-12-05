5a96b92cc22a3bdbf33400ad5a196df68e2186d6
Natalia Ramirez <68573508+NataliaCatalina@users.noreply.github.com>
2021-12-05 22:11:38 +1300

    user and home page style

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -65,13 +65,14 @@ export default function User () {
   return <>
 
     <div className="user__container">
-      <div className='user__container-red'>
-        <label> <img className='user-image' src={'images/' + user.image}/>
-        </label>
+      <div className='user__container-redRow'>
+        <div className='user__container-red'>
+          <img className='user-image' src={'images/' + user.image} />
         </div>
       </div>
       <div className='user__profile'>
         <div className='user__profile-info'>
+          <label><h2>{user.name}</h2></label><br />
           <div className="socialmedia">
             <label><a href={`${user.facebook}`}><i className="fab fa-facebook"></i></a></label>
             <label><a href={`${user.linkedin}`}><i className="fab fa-linkedin"></i></a></label>
@@ -79,29 +80,29 @@ export default function User () {
             <label><a href={`${user.instagram}`}><i className="fab fa-instagram"></i></a></label>
             <label><a href={`${user.githubLink}`}><i className="fab fa-github"></i></a></label>
           </div>
-          <label><h3>Name:</h3><h4>{user.name}</h4></label><br/>
-          <label><h3>Cohort:</h3><h4>{user.cohort}</h4></label><br/>
-          <label><h3>Email:</h3><h4>{user.email}</h4></label><br/>
-          <label><h3>Location:</h3><h4>{user.location}</h4></label><br/>
-          <label><h3>Quote:</h3><h4>{user.quote}</h4></label><br/>
-          <label><h3>Skill Set:</h3><h4>{user.skills}</h4></label><br/>
+          <label><h3>Cohort:</h3><h4>{user.cohort}</h4></label><br />
+          <label><h3>Email:</h3><h4>{user.email}</h4></label><br />
+          <label><h3>Location:</h3><h4>{user.location}</h4></label><br />
+          <label><h3>Quote:</h3><h4>{user.quote}</h4></label><br />
+          <label><h3>Skill Set:</h3><h4>{user.skills}</h4></label><br />
         </div>
         {/* <div className='use__profile-middle'>
         </div> */}
+
         <div className='user__profileImage-form'>
           <form onSubmit={onFormSubmit}>
             <div className='Image-Upload'><h3 style={{ textDecoration: 'underline' }}>Hover For Upload Image Info</h3>
-              <span className='Image-Upload-Text'><p>When you upload your image <br/>please make sure the filename is <em>your-name</em>.jpg <br/>starting with a capital letter - eg David.jpg or JV.jpg. <br/>This will ensure the image will update<br/> properly in the database.</p></span><br/>
+              <span className='Image-Upload-Text'><p>When you upload your image <br />please make sure the filename is <em>your-name</em>.jpg <br />starting with a capital letter - eg David.jpg or JV.jpg. <br />This will ensure the image will update<br /> properly in the database.</p></span><br />
             </div>
-            <input className='file' type='file' name='profilePic' onChange={onInputChange} /><br/><br/>
+            <input className='file' type='file' name='profilePic' onChange={onInputChange} /><br /><br />
             <button className='edit' type="submit">Upload</button>
           </form>
           <div className='user__profileUpdate-form'>
-            <UpdateUser id={user.id} updateTheUser={updateTheUser} user={user}/>
+            <UpdateUser id={user.id} updateTheUser={updateTheUser} user={user} />
           </div>
         </div>
-
       </div>
-  
+
+    </div>
   </>
 }
