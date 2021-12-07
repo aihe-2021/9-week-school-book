854e8084d6a14f3b4d29e239f8b7a47414741501
Danny <Danny@Apples-iMac.local>
2021-12-07 19:30:26 +1300

    image upload instructions

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -53,7 +53,10 @@ export default function User (props) {
       <h1>{user.name}</h1>
     </div>
     <div className='user__container-image'>
-      {user.image && <img className='user-image' src={user.image} />}
+      <div className="container-image">
+        {user.image && <img className='user-image' src={user.image} />}
+        <div className='overlay-text content'>To change your profile picture <br/> update it on <a className="githubProfile" href="https://github.com/settings/profile">Github.</a></div>
+      </div>
       <div className="socialmedia">
         <label><a href={`${user.facebook}`}><i className="fab fa-facebook"></i></a></label>
         <label><a href={`${user.linkedin}`}><i className="fab fa-linkedin"></i></a></label>
