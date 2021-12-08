b61682ca691cef72454f7cd5b97b9c5489b9d027
Danny <Danny@Apples-iMac.local>
2021-12-09 10:06:30 +1300

    final push

--- server/db/seeds/comments.js
+++ server/db/seeds/comments.js
@@ -11,20 +11,6 @@ exports.seed = (knex) => {
           id: 1,
           userId: 1
         }
-        // {
-        //   id: 2,
-        //   userId: 2,
-        //   comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2',
-        //   date_posted: new Date(Date.now()),
-        //   comment: 'Really Great person'
-        // },
-        // {
-        //   id: 3,
-        //   userId: 3,
-        //   comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2',
-        //   date_posted: new Date(Date.now()),
-        //   comment: 'Extremely great person'
-        // }
       ])
     })
 }
