47f9495d243227713a0fe731ac9945a73c9a877f
Danny <Danny@Apples-iMac.local>
2021-12-08 17:13:11 +1300

    heroku stuff

--- server/db/migrations/20211206212408_comments.js
+++ server/db/migrations/20211206212408_comments.js
@@ -2,7 +2,7 @@ exports.up = (knex) => {
   return knex.schema.createTable('comments', (table) => {
     table.increments('id').primary()
     table.integer('userId')
-    table.integer('comment_by_user') // this is auth id
+    table.string('comment_by_user') // this is auth id
     table.date('date_posted')
     table.string('comment')
   })
