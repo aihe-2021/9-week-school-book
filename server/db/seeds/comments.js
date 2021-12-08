a48556c36a81548a719dbdc3e150d8fcf18a3323
Danny <Danny@Apples-iMac.local>
2021-12-08 17:39:41 +1300

    heroku

--- server/db/seeds/comments.js
+++ server/db/seeds/comments.js
@@ -7,23 +7,23 @@ exports.seed = (knex) => {
         {
           id: 1,
           userId: 1,
+          comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2',
           date_posted: new Date(Date.now()),
-          comment: 'Great person',
-          comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2'
+          comment: 'Great person'
         },
         {
           id: 2,
           userId: 2,
+          comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2',
           date_posted: new Date(Date.now()),
-          comment: 'Really Great person',
-          comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2'
+          comment: 'Really Great person'
         },
         {
           id: 3,
           userId: 3,
+          comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2',
           date_posted: new Date(Date.now()),
-          comment: 'Extremely great person',
-          comment_by_user: 'nsVl526ydneLlx2zCLQnlsZGm7R2'
+          comment: 'Extremely great person'
         }
       ])
     })
